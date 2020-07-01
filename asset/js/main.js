20dd3b071e07f4ab6dd630727317112729a1bb6b
Yunus Emre Ördek <yunus9705@gmail.com>
2020-07-01 14:33:09 +0300

    Header-switch mobil-tablet-pc özellikleri eklendi. mobil ve tablet için switch tasarım ayarı yapıldı.

---
+++ asset/js/main.js
@@ -0,0 +1,9 @@
+$('.Header-switch').on('click', function () {
+    
+    if($('.Header').is('.menu-show')){
+        $('.Header').removeClass('menu-show')
+    }
+    else{
+        $('.Header').addClass('menu-show')
+    }
+  })
